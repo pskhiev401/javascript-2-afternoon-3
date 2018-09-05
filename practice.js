b4f388479722a27f2c576777c471b3c95ece7f23
Patric Khiev <r@Rs-MacBook-Pro.local>
2018-09-04 22:42:24 -0500

    project finished

--- practice.js
+++ practice.js
@@ -28,6 +28,10 @@
 */
 
 // Code Here 
+function first(arr, cb){
+  cb(arr[0]);
+}
+
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +52,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb){
+  cb(arr[arr.length-1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,7 +73,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1, num2, cb){
+  cb(num1 * num2);
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,6 +94,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, person, cb){
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] === person){
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +124,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, cb){
+  for(let i = arr.length-1; i >= 0; i--){
+    if (i !== arr.indexOf(arr[i])) {
+      arr.splice(i, 1);
+    }
+      cb(arr)
+  }
+  return arr;
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +150,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr,cb){
+  for(let i = 0; i < arr.length; i++){
+    cb(arr[i], i)
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +172,13 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users, id, cb){
+  for(let i = 0; i<users.length; i++){
+    if(users[i].id === id){
+      cb(users[i])
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
